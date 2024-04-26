fef7ec17ee223f2df580c68cd43a3b36d5d48dcd
Christoph Wieser <christoph.wieser@edu.fh-joanneum.at>
2024-04-26 11:51:37 +0200

    add DB trip migration + merging

--- AcademiCar.Server/DAL/UnitOfWork/PostgresDbContext.cs
+++ AcademiCar.Server/DAL/UnitOfWork/PostgresDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using AcademiCar.Server.DAL.Entities;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 
 namespace AcademiCar.Server.DAL.UnitOfWork
@@ -22,19 +21,25 @@ namespace AcademiCar.Server.DAL.UnitOfWork
             modelBuilder.Entity<Preferences>().ToTable("Preferences", schema: "academicar");
             modelBuilder.Entity<Rating>().ToTable("Rating", schema: "academicar");
             modelBuilder.Entity<Vehicle>().ToTable("Vehicle", schema:"academicar");
+            modelBuilder.Entity<Trip>().ToTable("Trip", schema: "academicar");
+            modelBuilder.Entity<TripRequest>().ToTable("TripRequest", schema: "academicar");
+            modelBuilder.Entity<Address>().ToTable("Adress", schema: "academicar");
         }
 
 
         #region DB Tables
 
         // TODO - insert tables here
-        // Example:
         public DbSet<TestTable> TestTableEntries { get; set; }
         public DbSet<Stats> Stats { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<FavoriteUser> FavoriteUsers { get; set; }
         public DbSet<Preferences> Preferences { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<TripRequest> TripRequests { get; set; }
+        public DbSet<Address> Addresses { get; set; }
         public DbSet<Vehicle> Vehicles { get; set; }
+        
         #endregion
     }
 }
--- AcademiCar.Server/Migrations/20240426094637_CST-132-trips.Designer.cs
+++ AcademiCar.Server/Migrations/20240426094637_CST-132-trips.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AcademiCar.Server.DAL.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AcademiCar.Server.Migrations
 {
     [DbContext(typeof(PostgresDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240426094637_CST-132-trips")]
+    partial class CST132trips
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
