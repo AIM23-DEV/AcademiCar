69ddc62daae3a1ded4e6478e05a3f3b97be88eaa
haringma15 <martin.haring2@edu.fh-joanneum.at>
2024-05-07 00:28:08 +0200

    CST-150: Add base test, add crud test, install nunit

---
+++ AcademiCar.Server/Tests/BaseClasses/BaseUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using AcademiCar.Server.DAL.UnitOfWork;
+using NUnit.Framework;
+
+namespace AcademiCar.Server.Tests.BaseClasses
+{
+    [TestFixture]
+    public class BaseUnitTest
+    {
+        protected ServiceCollection _services;
+        protected ServiceProvider _serviceProvider;
+
+        protected IUnitOfWork _unitOfWork;
+
+
+        [SetUp]
+        public virtual async Task Setup()
+        {
+            _services = new ServiceCollection();
+            _CollectServices();
+            _serviceProvider = _services.BuildServiceProvider();
+
+            _unitOfWork = _serviceProvider.GetRequiredService<IUnitOfWork>();
+        }
+
+        [TearDown]
+        public virtual async Task TearDown()
+        {
+            _serviceProvider.Dispose();
+        }
+
+
+        protected virtual void _CollectServices()
+        {
+            _services.AddSingleton<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
---
+++ AcademiCar.Server/Tests/DAL/PostgresRepositoryTest.cs
@@ -0,0 +1,112 @@
+﻿using AcademiCar.Server.DAL.Entities;
+using AcademiCar.Server.Tests.BaseClasses;
+using NUnit.Framework;
+
+namespace AcademiCar.Server.Tests.DAL
+{
+    public class PostgresRepositoryTest : BaseUnitTest
+    {
+        const string TEST_EMAIL = "postgre@repo.test";
+
+
+        public override async Task Setup()
+        {
+            await base.Setup();
+
+            await _unitOfWork.Users.InsertAsync(new User() { Email = TEST_EMAIL });
+        }
+        public override async Task TearDown()
+        {
+            await base.TearDown();
+
+            await _unitOfWork.Users.DeleteAsync(u => u.Email == TEST_EMAIL);
+        }
+
+
+        [Test]
+        [TestCase(ExpectedResult = true)]
+        public bool TestFilterBy()
+        {
+            try
+            {
+                if (!_unitOfWork.Users.FilterBy(u => u.Email == TEST_EMAIL).Any()
+                || !_unitOfWork.Users.FilterBy(u => u.Email == TEST_EMAIL, a => (object)a).Any())
+                    throw new Exception("FilterBy Error!");
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        [Test]
+        [TestCase(ExpectedResult = true)]
+        public async Task<bool> TestFind()
+        {
+            try
+            {
+                User testUser = await _GetUserByEmail(TEST_EMAIL);
+                if (testUser.Email != TEST_EMAIL) throw new Exception("FindAsync Error!");
+
+                User idTestUser = await _unitOfWork.Users.FindByIdAsync(testUser.ID);
+                if (idTestUser.Email != TEST_EMAIL) throw new Exception("FindById Error!");
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        [Test]
+        [TestCase(ExpectedResult = true)]
+        public async Task<bool> TestCRUD()
+        {
+            try
+            {
+                int initialUserCount = _GetCurrentUserCount();
+
+                await _unitOfWork.Users.InsertAsync(new User() { Email = "insert@crud.test" });
+                await _unitOfWork.Users.InsertAsync(new User() { Email = "delete@crud.test" });
+                
+                if (!(_GetCurrentUserCount() == initialUserCount + 2))
+                    throw new Exception("InsertAsync Error!");
+
+                User insertedUser = await _GetUserByEmail("insert@crud.test");
+                if (insertedUser.Email != "insert@crud.test")
+                    throw new Exception("FindAsync Error!");
+
+                insertedUser.Email = "update@crud.test";
+                await _unitOfWork.Users.UpdateAsync(insertedUser);
+                User updatedUser = await _GetUserByEmail("update@crud.test");
+                if (updatedUser.ID != insertedUser.ID)
+                    throw new Exception("UpdateAsync Error!");
+
+                await _unitOfWork.Users.DeleteAsync(u => u.Email == "delete@crud.test");
+                if (!(_GetCurrentUserCount() == initialUserCount + 1))
+                    throw new Exception("DeleteAsync Error!");
+
+                await _unitOfWork.Users.DeleteByIdAsync(insertedUser.ID);
+                if (!(_GetCurrentUserCount() == initialUserCount))
+                    throw new Exception("DeleteById Error!");
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        private async Task<User> _GetUserByEmail(string email)
+            => await _unitOfWork.Users.FindAsync(u => u.Email == email);
+        private int _GetCurrentUserCount()
+            => _unitOfWork.Users.FilterBy(u => u.Email.Contains("@crud.test")).Count();
+    }
+}
