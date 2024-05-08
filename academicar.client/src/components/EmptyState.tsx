7f0a448c50f40998f153f896ac8aa19b081cfefc
Andreas Feichtinger <anfeichtingers@gmail.com>
2024-05-08 12:27:37 +0200

    CST-153: implement EmptyState component

---
+++ academicar.client/src/components/EmptyState.tsx
@@ -0,0 +1,35 @@
+﻿import {ReactNode} from "react";
+import {BiQuestionMark} from "react-icons/bi";
+
+interface EmptyStateProps {
+    icon?: ReactNode
+    title?: string | ReactNode
+    subtitle?: string | ReactNode
+    children?: ReactNode
+    asCard?: boolean
+    className?: string
+}
+
+export const EmptyState = (props: EmptyStateProps) => {
+    return (
+        <div
+            className={'inline-flex flex-col items-center justify-center text-center w-full' + (props.asCard ? ' bg-white rounded-xl px-4 py-6 shadow-sm' : '') + ((props.className ? (' ' + props.className) : ''))}>
+
+            <div
+                className={'flex items-center justify-center rounded-full text-gray-400'}>
+                {props.icon ? props.icon : <BiQuestionMark className={props.asCard ? 'h-14 w-14' : 'h-16 w-16'}/>}
+            </div>
+
+            <h4 className={'mt-4 ' + (props.asCard ? 'headline-4' : 'headline-2')}>
+                {props.title ? props.title : 'Keine Ergebnisse'}
+            </h4>
+
+            <p className={'mb-4 ' + (props.asCard ? 'body-1' : 'headline-3')}>
+                {props.subtitle ? props.subtitle : ''}
+            </p>
+
+            {props.children}
+
+        </div>
+    );
+}
