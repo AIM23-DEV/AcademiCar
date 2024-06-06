0f3d151a94b9ff1e349e472b7ccc8cb0fe6da108
Christoph Wieser <christoph.wieser@edu.fh-joanneum.at>
2024-06-06 09:58:30 +0200

    fix dev build

--- academicar.client/vite.config.ts
+++ academicar.client/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
+import child_process from 'child_process';
 import { env } from 'process';
 
 const baseFolder =
@@ -15,6 +16,17 @@ const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
 if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+    if (0 !== child_process.spawnSync('dotnet', [
+        'dev-certs',
+        'https',
+        '--export-path',
+        certFilePath,
+        '--format',
+        'Pem',
+        '--no-password',
+    ], { stdio: 'inherit', }).status) {
+        throw new Error("Could not create certificate.");
+    }
     throw new Error("Certificate files do not exist.");
 }
 
