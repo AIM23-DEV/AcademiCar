b42d3e35e99beaa36721ec558ce14ed3052f4354
Daniel Koelbl <daniel.koelbl@edu.fh-joanneum.at>
2024-06-06 20:22:39 +0200

    CST-162: Trip Detail Page

--- academicar.client/src/routes/TripRoutes.tsx
+++ academicar.client/src/routes/TripRoutes.tsx
@@ -1,6 +1,6 @@
 ﻿import {Outlet, Route} from 'react-router-dom';
 import {IndexTripsPage} from "../pages/trips/IndexTripsPage.tsx";
-import {ShowTripPage} from "../pages/search/ShowTripPage.tsx";
+import {ShowTripPage} from "../pages/trips/ShowTripPage.tsx";
 import {CreateTripPage} from "../pages/trips/CreateTripPage.tsx";
 
 // All routes are prefixed with /trips.
