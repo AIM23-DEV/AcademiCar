7a15dfc82ec21986195b48a437d792c86679490a
Raphael Klein <kleinrap@gmail.com>
2024-06-13 12:19:46 +0200

    Implemented Sort and Order function

--- academicar.client/src/routes/AdminRoutes.tsx
+++ academicar.client/src/routes/AdminRoutes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Outlet, Route } from 'react-router-dom';
-import { useAuth } from '../AuthContext';
+import { Outlet, Route} from 'react-router-dom';
+//import { useAuth } from '../AuthContext';
 import Dashboard from "../pages/auth/Dashboard";
 import AdminLogin from "../pages/auth/AdminLogin.tsx";
 import {IndexUsersPage} from "../pages/admin/IndexUsersPage.tsx";
@@ -9,15 +9,15 @@ import {ShowUserPage} from "../pages/admin/ShowUserPage.tsx";
 // Todo define admin routes!
 // All routes are prefixed with /admin.
 
-//<Route path="users" element={<IndexUsersPage />} />
+<Route path="users" element={<IndexUsersPage />} />
 const AdminPrivateRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const { user } = useAuth();
+    //const { user } = useAuth();
 
-    if (!user) {
+    /*if (!user) {
         return <Route path="login" element={<AdminLogin />} />;
     }
 
-    /*if (!user.roles || !user.roles.includes('admin')) {
+    if (!user.roles || !user.roles.includes('admin')) {
         return <Navigate to="/admin-login" />;
     }*/
 
