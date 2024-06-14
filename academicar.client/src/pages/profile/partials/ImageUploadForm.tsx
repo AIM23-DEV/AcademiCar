1e0289a8ec257aafea185d8a609802dde556321e
kemara_23 <marina.rampler@gmail.com>
2024-06-15 00:38:30 +0200

    ImageUploadForm

--- academicar.client/src/pages/profile/partials/ImageUploadForm.tsx
+++ academicar.client/src/pages/profile/partials/ImageUploadForm.tsx
@@ -1,106 +1,14 @@
 import {ChangeEvent, useState} from "react";
 import {Button} from "../../../components/Buttons.tsx";
 import {Card} from "../../../components/Cards.tsx";
-import axios, {AxiosResponse} from "axios";
+import {BlobServiceClient} from "@azure/storage-blob";
 
-// Used only for local development
-const API_SERVER = import.meta.env.VITE_API_SERVER as string;
 
-const request = axios.create({
-    baseURL: API_SERVER,
-    headers: {
-        'Content-type': 'application/json'
-    }
-});
-
-type SasResponse = {
-    url: string;
-};
 export const ImageUploadForm = () => {
-    const [selectedFile, setSelectedFile] = useState<File | null>(null);
-    const [sasTokenUrl, setSasTokenUrl] = useState<string>('');
-    const [uploadStatus, setUploadStatus] = useState<string>('');
+    const [selectedFile, setSelectedFile] = useState<File|null>(null);
     const [list] = useState<string[]>([]);
-
-    /*
-    const uploadProfileImage = async () => {
-        try{
-            //  const promises = [];
-            if (sasTokenUrl === ''){
-                // return;
-                console.log("set SAS token ");
-                setSasTokenUrl("https://academicar.blob.core.windows.net/?sv=2022-11-02&ss=bfqt&srt=o&sp=rwdlacupiytfx&se=2024-06-09T21:23:51Z&st=2024-06-09T13:23:51Z&spr=https&sig=GCh56CQUO%2FwS%2BSzRVBzHiEZBjrqgZMqjJrnT1CjVe3I%3D")
-            } 
-
-            convertFileToArrayBuffer(selectedFile as File)
-                .then((fileArrayBuffer) => {
-                    if (
-                        fileArrayBuffer === null ||
-                        fileArrayBuffer.byteLength < 1 ||
-                        fileArrayBuffer.byteLength > 256000
-                    )
-                        return;
-
-                    const blockBlobClient = new BlockBlobClient(sasTokenUrl);
-                    return blockBlobClient.uploadData(fileArrayBuffer);
-                })
-                .then(() => {
-                    setUploadStatus('Successfully finished upload');
-                    return request.get(`/api/list?container=${containerName}`);
-                })
-                .then((result: AxiosResponse<ListResponse>) => {
-                    // Axios response
-                    const { data } = result;
-                    const { list } = data;
-                    setList(list);
-                })
-        }
-        catch (error){
-            // @ts-ignore
-            alert(error.message);
-            // @ts-ignore
-            if (error instanceof  Error) {
-                setUploadStatus(
-                    `Failed to finish upload with error : ${error.message}`
-                );
-            } else {
-                setUploadStatus(error as string)
-            }
-        }
-    };
-    */
+        // Other component code...
     
-    const handleFileSasToken = () => {
-        //   const permission = 'w'; //write
-        //   const timerange = 5; //minutes
-
-        if (!selectedFile) return;
-
-        request
-            .put(
-                `https://academicar.blob.core.windows.net/?sv=2022-11-02&ss=bfqt&srt=o&sp=rwdlacupiytfx&se=2024-06-09T21:23:51Z&st=2024-06-09T13:23:51Z&spr=https&sig=GCh56CQUO%2FwS%2BSzRVBzHiEZBjrqgZMqjJrnT1CjVe3I%3D`,
-                {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-            )
-            .then((result: AxiosResponse<SasResponse>) => {
-                const { data } = result;
-                const { url } = data;
-                setSasTokenUrl(url);
-            })
-            .catch((error: unknown) => {
-                if (error instanceof Error) {
-                    const { message, stack } = error;
-                    setSasTokenUrl(`Error getting sas token: ${message} ${stack || ''}`);
-                } else {
-                    setUploadStatus(error as string);
-                }
-            });
-    };
-
-
     const handleFileSelection = (event: ChangeEvent<HTMLInputElement>) => {
         const { target } = event;
 
@@ -113,40 +21,49 @@ export const ImageUploadForm = () => {
             return;
 
         setSelectedFile(target?.files[0]);
-
-        // reset
-        setSasTokenUrl('');
-        setUploadStatus('');
+        console.log(`Selected file: ${selectedFile}`);
     };
+
+    async function handleUpload() {
+
+        console.log(`handleUpload: ${selectedFile}`);
+        const connectionString = 'DefaultEndpointsProtocol=https;AccountName=academicar;AccountKey=mNaipDioJQ1IoDwVaR7BKDXgm+RYRX6IqlW4dXBvkBA63yOpteGM8jqUWAF4nEMiURmrPf43XphD+AStZeKFtA==;EndpointSuffix=core.windows.net';
+        const containerName = 'academicar';
+
+        const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+        console.log(`blobServiceClient-accountName: ${blobServiceClient.accountName}`);
+        console.log(`blobServiceClient-url: ${blobServiceClient.url}`);
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        console.log(`containerClient-accountName: ${containerClient.accountName}`);
+        console.log(`containerClient-containerName: ${containerClient.containerName}`);
+        console.log(`containerClient-url: ${containerClient.url}`);
+
+        if(selectedFile == null)
+            return;
+        
+// Example for file upload
+            const blobName = `photos/${selectedFile.name}`;
+            console.log(`Uploading with blobname: ${blobName}`);
+            const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+            await blockBlobClient.uploadData(selectedFile); 
+        }
+    
 
     return (
         <Card label="Suche" className="mt-6">
             <form aria-label="Suche" className="w-full grid grid-cols-12 gap-4">
                 <input type="file" className={"col-span-full"} onChange={handleFileSelection} />
 
-                <Button
-                    variant={"secondary"}
-                    text={"Get SAS Token"}
-                    className={"col-span-full"}
-                    onClick={handleFileSasToken}
-                />
-
-                {sasTokenUrl && (
-                    <div className="body-2">SAS-Token URL: {sasTokenUrl}</div>
-                )}
+             
                 
                 <Button
                     variant={"primary"}
                     text={"Upload Image"}
                     type={"submit"}
                     className={"col-span-full"}
+                    onClick={handleUpload}
                 />
-
-                {uploadStatus && (
-                    <div className="body-2">
-                        {uploadStatus}
-                    </div>
-                )}
+                
 
                 {list.map((item) => (
                     
