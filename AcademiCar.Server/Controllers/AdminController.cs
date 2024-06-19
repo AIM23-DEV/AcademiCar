97c35e4b4e6f6cbfa0ada7b3d7564d189ee5d803
fhs45918 <fhs45918@fh-salzburg.ac.at>
2024-06-19 17:37:35 +0200

    CST-207: Add database data of ratings

--- AcademiCar.Server/Controllers/AdminController.cs
+++ AcademiCar.Server/Controllers/AdminController.cs
@@ -42,4 +42,50 @@ public class AdminController : ControllerBase
         Stats? userStats  = await _globalService.StatsService.Get(idAsInt);
         return Ok(userStats);
     }
+    
+    [HttpGet("users/rating/{id}")]
+    public async Task<IActionResult> GetUserRating(string id)
+    {
+        List<Rating?> userRatings  = await _globalService.RatingService.Get();
+        
+        if (userRatings.Count == 0)
+        {
+            return NotFound();
+        }; 
+        
+        List<Rating> FilterdUserRatings = userRatings.Where(r => r != null && r.FK_RatedUser == id).ToList();
+
+        float sumRating = 0;
+
+        foreach (Rating rating in FilterdUserRatings)
+        {
+            sumRating += rating.Score;
+        }
+        TotalRating totalRating = new (); 
+            
+        totalRating.Rating  = sumRating / FilterdUserRatings.Count();
+        
+        return Ok(totalRating);
+    }
+    
+    [HttpGet("users/review/{id}")]
+    public async Task<ActionResult<User>> GetUserReviews(string id)
+    {
+        List<Rating?> userRatings  = await _globalService.RatingService.Get();
+        
+        if (userRatings.Count == 0)
+        {
+            return NotFound();
+        }; 
+        
+        List<Rating> FilterdUserRatings = userRatings.Where(r => r != null && r.FK_RatedUser == id).ToList();
+        
+        return Ok(FilterdUserRatings.Count());
+    }
+}
+
+class TotalRating
+{
+    public float Rating { get; set; }
+
 }
--- academicar.client/src/pages/profile/StatsPage.tsx
+++ academicar.client/src/pages/profile/StatsPage.tsx
@@ -9,16 +9,15 @@ import {Card} from "../../components/Cards.tsx";
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 
-const DATA = {
-    rating: {
-        stars: 5.0,
-        count: 5
-    },
+interface TotalRating {
+    rating: number;
 }
 export const StatsPage = () => {
     const { id } = useParams();
     const [user, setUser] = useState<IUser>();
     const [stats, setStats] = useState<IStats>();
+    const [rating, setRating] = useState<TotalRating | null>();
+    const [review, setReview] = useState<number | null>();
     const [error, setError] = useState<string | null>();
     const [t] = useTranslation();
     const pageTitle = t("pages/profile:StatsPage.title");
@@ -48,6 +47,28 @@ export const StatsPage = () => {
     }
 
     if (!stats) return <div>Loading stats...</div>;
+
+    if (user && !rating) {
+        fetch(`https://localhost:5173/api/admin/users/rating/${user.id}`)
+            .then(response => response.json())
+            .then(data => setRating(data))
+            .catch(error => {
+                setError("There was an error fetching the Admin rating!");
+                console.error(error);
+            });
+    }
+    if (!rating) return <div>Loading stats...</div>;
+    
+    if (user && !review) {
+        fetch(`https://localhost:5173/api/admin/users/review/${user.id}`)
+            .then(response => response.json())
+            .then(data => setReview(data))
+            .catch(error => {
+                setError("There was an error fetching the Admin rating!");
+                console.error(error);
+            });
+    }
+    if (!setReview) return <div>Loading stats...</div>;
     
     return (
         <>
@@ -63,20 +84,20 @@ export const StatsPage = () => {
                         />
                     </div>
                     
-                    <div className="headline-2">{user.firstName} {user.lastName}</div>
+                    <div className="headline-2 flex justify-center">{user.firstName} {user.lastName}</div>
                     
                     <div className="flex justify-center">
-                        {Array.from({length: Math.floor(DATA.rating.stars) }).map((_, idx) => (
+                        {Array.from({length: Math.floor(rating.rating) }).map((_, idx) => (
                             <BiSolidStar key={idx} className="icon text-yellow-400" />
                         ))}
-                        {Array.from({ length: 5 - Math.floor(DATA.rating.stars) }).map((_, idx) => (
+                        {Array.from({ length: 5 - Math.floor(rating.rating) }).map((_, idx) => (
                             <BiSolidStar key={idx} className="icon text-gray-300" />
                         ))}
                     </div>
                     
                     <div className="flex justify-center">
                         <TextButton
-                            text={DATA.rating.count + " " +  t("pages/profile:StatsPage.ratings") + " (" + DATA.rating.stars + ")"}
+                            text={review + " " +  t("pages/profile:StatsPage.ratings") + " (" + rating.rating + ")"}
                             trailing={<BiChevronRight className="icon" />}
                         />
                     </div>
