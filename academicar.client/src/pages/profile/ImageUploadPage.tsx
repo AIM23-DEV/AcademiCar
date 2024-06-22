12ead850384285db008f12f814d2b337bcab09eb
kemara_23 <marina.rampler@gmail.com>
2024-06-22 10:43:38 +0200

    image upload blob view

--- academicar.client/src/pages/profile/ImageUploadPage.tsx
+++ academicar.client/src/pages/profile/ImageUploadPage.tsx
@@ -2,6 +2,7 @@ import SetPageTitle from "../../hooks/set_page_title.tsx";
 import {TitleBar} from "../../components/TitleBar.tsx";
 import {BottomNavigationBar} from "../../components/BottomNavigationBar.tsx";
 import {ImageUploadForm} from "./partials/ImageUploadForm.tsx";
+import { BlobView } from "./partials/BlobView.tsx";
 
 export const ImageUploadPage = () => {
     SetPageTitle("Image Upload");
@@ -12,6 +13,7 @@ export const ImageUploadPage = () => {
             
             <div className="w-full flex flex-col items-center justify-center pt-32">
             <ImageUploadForm/>
+            <BlobView/>
             </div>
             
             <BottomNavigationBar />
---
+++ academicar.client/src/pages/profile/partials/BlobView.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+// we'll need InteractiveBrowserCredential here to force a user to sign-in through the browser
+import { InteractiveBrowserCredential } from "@azure/identity";
+// we're using these objects from the storage sdk - there are others for different needs
+import { BlobServiceClient, BlobItem } from "@azure/storage-blob";
+
+interface Props {}
+interface State {
+    // a place to store our blob item metadata after we query them from the service
+    blobsWeFound: BlobItem[];
+    containerUrl: string;
+}
+
+export class BlobView extends React.Component<Props, State> {
+    state: State;
+
+    constructor(props: Props, state: State) {
+        //super(state);
+        super(props, state);
+        this.state = { blobsWeFound: [], containerUrl: "" }
+    }
+
+    // here's our azure identity config
+    async componentDidMount() {
+        const signInOptions = {
+            // the client id is the application id, from your earlier app registration
+            clientId: "6a88bd44-fb43-4e2e-be79-7df666b70b23",
+            // this is your tenant id - the id of your azure ad tenant. available from your app registration overview
+            tenantId: "4caa5dc8-1da7-4c89-8c89-dc816e05f20b"
+        }
+
+        const blobStorageClient = new BlobServiceClient(
+            // this is the blob endpoint of your storage acccount. Available from the portal 
+            // they follow this format: <accountname>.blob.core.windows.net for Azure global
+            // the endpoints may be slightly different from national clouds like US Gov or Azure China
+            "https://<your storage account name>.blob.core.windows.net/",
+            new InteractiveBrowserCredential(signInOptions)
+        )
+
+        // this uses our container we created earlier - I named mine "private"
+        var containerClient = blobStorageClient.getContainerClient("private");
+        var localBlobList = [];
+        // now let's query our container for some blobs!
+        for await (const blob of containerClient.listBlobsFlat()) {
+            // and plunk them in a local array...
+            localBlobList.push(blob);
+        }
+        // ...that we push into our state
+        this.setState({ blobsWeFound: localBlobList, containerUrl: containerClient.url });
+    }
+
+    render() {
+        return (
+            <div>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>blob name</th>
+                        <th>blob size</th>
+                        <th>download url</th>
+                    </tr>
+                    </thead>
+                    <tbody>{
+                        this.state.blobsWeFound.map((x, i) => {
+                            return <tr key={i}>
+                                <td>{x.name}</td>
+                                <td>{x.properties.contentLength}</td>
+                                <td>
+                                    <img src={this.state.containerUrl + x.name} />
+                                </td>
+                            </tr>
+                        })
+                    }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
