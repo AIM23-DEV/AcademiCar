1239ee390dd23424b0d3ec96a8e8a4b49720da18
kemara_23 <marina.rampler@gmail.com>
2024-06-22 12:23:38 +0200

    image upload blob view

--- academicar.client/src/pages/profile/partials/BlobView.tsx
+++ academicar.client/src/pages/profile/partials/BlobView.tsx
@@ -1,93 +1,140 @@
-import React from 'react';
+import{ChangeEvent} from 'react';
 // we'll need InteractiveBrowserCredential here to force a user to sign-in through the browser
 //import { DefaultAzureCredential  } from "@azure/identity";
 // we're using these objects from the storage sdk - there are others for different needs
-import { BlobServiceClient, BlobItem } from "@azure/storage-blob";
+import { BlobServiceClient,  } from "@azure/storage-blob";
+import {Button} from "../../../components/Buttons.tsx";
 
-interface Props {}
-interface State {
-    // a place to store our blob item metadata after we query them from the service
-    blobsWeFound: BlobItem[];
-    containerUrl: string;
-}
 
-export class BlobView extends React.Component<Props, State> {
-    state: State;
 
-    constructor(props: Props, state: State) {
-        //super(state);
-        super(props, state);
-        this.state = { blobsWeFound: [], containerUrl: "" }
-    }
+export const BlobView =() => {
+   
 
-    // here's our azure identity config
-    async componentDidMount() {
- /*       const signInOptions = {
-            // the client id is the application id, from your earlier app registration
-            clientId: "6a88bd44-fb43-4e2e-be79-7df666b70b23",
-            // this is your tenant id - the id of your azure ad tenant. available from your app registration overview
-            tenantId: "4caa5dc8-1da7-4c89-8c89-dc816e05f20b"
+
+        const handleFileSelection = (event: ChangeEvent<HTMLInputElement>) => {
+            console.log('handleFileSelection');
+            const {target} = event;
+
+
+            if (!(target instanceof HTMLInputElement)) return;
+            if (
+                target?.files === null ||
+                target?.files?.length === 0 ||
+                target?.files[0] === null
+            )
+                return;
+
+            if (target.files.length >= 0) {
+                //  setSelectedFile(target.files[0]);
+                console.log(`target: items = ${target.files?.length}`)
+                console.log(`target: name = ${target.files[0]?.name}\ntype = ${target.files[0]?.type}`)
+                handleUpload(target?.files[0]).then(r => {
+                    console.log(`promise...`);
+                    if (r === null)
+                        return;
+                    console.log(`promise: ${typeof (r)}`);
+                  
+                });
+            }
+
+
         }
-
-        const blobStorageClient = new BlobServiceClient(
-            // this is the blob endpoint of your storage acccount. Available from the portal 
-            // they follow this format: <accountname>.blob.core.windows.net for Azure global
-            // the endpoints may be slightly different from national clouds like US Gov or Azure China
-            "https://academicar.blob.core.windows.net/",
-            new DefaultAzureCredential()
-        
-        );*/
+    
+    
+  
+    async function handleUpload(selectedFile:File) {
 
         const AZURE_STORAGE_CONNECTION_STRING =
-           "DefaultEndpointsProtocol=https;AccountName=academicar;AccountKey=mNaipDioJQ1IoDwVaR7BKDXgm+RYRX6IqlW4dXBvkBA63yOpteGM8jqUWAF4nEMiURmrPf43XphD+AStZeKFtA==;EndpointSuffix=core.windows.net";
+            "DefaultEndpointsProtocol=https;AccountName=academicar;AccountKey=mNaipDioJQ1IoDwVaR7BKDXgm+RYRX6IqlW4dXBvkBA63yOpteGM8jqUWAF4nEMiURmrPf43XphD+AStZeKFtA==;EndpointSuffix=core.windows.net";
 
-      /*  if (!AZURE_STORAGE_CONNECTION_STRING) {
-            throw Error('Azure Storage Connection string not found');
-        }
-*/
+        /*  if (!AZURE_STORAGE_CONNECTION_STRING) {
+              throw Error('Azure Storage Connection string not found');
+          }
+  */
 // Create the BlobServiceClient object with connection string
-        const blobServiceClient = BlobServiceClient.fromConnectionString(
+        const blobServiceClient1 = BlobServiceClient.fromConnectionString(
             AZURE_STORAGE_CONNECTION_STRING
         );
 
-       // console.log(`token = ${defaultCredentials.ar()}`)
+        // console.log(`token = ${defaultCredentials.ar()}`)
         // this uses our container we created earlier - I named mine "private"
-        var containerClient = blobServiceClient.getContainerClient("private");
+        var containerClient1 = blobServiceClient1.getContainerClient("profile-images");
         var localBlobList = [];
         // now let's query our container for some blobs!
-        for await (const blob of containerClient.listBlobsFlat()) {
+        for await (const blob of containerClient1.listBlobsFlat()) {
             // and plunk them in a local array...
             localBlobList.push(blob);
         }
         // ...that we push into our state
-        this.setState({ blobsWeFound: localBlobList, containerUrl: containerClient.url });
-    }
 
-    render() {
-        return (
-            <div>
-                <table>
-                    <thead>
-                    <tr>
-                        <th>blob name</th>
-                        <th>blob size</th>
-                        <th>download url</th>
-                    </tr>
-                    </thead>
-                    <tbody>{
-                        this.state.blobsWeFound.map((x, i) => {
-                            return <tr key={i}>
-                                <td>{x.name}</td>
-                                <td>{x.properties.contentLength}</td>
-                                <td>
-                                    <img src={this.state.containerUrl + x.name} />
-                                </td>
-                            </tr>
-                        })
-                    }
-                    </tbody>
-                </table>
-            </div>
-        )
+
+        console.log(`selectedFile: ${selectedFile.name}`);
+        console.log(`handleUpload`);
+
+        const connectionString = 'BlobEndpoint=https://academicar.blob.core.windows.net/;QueueEndpoint=https://academicar.queue.core.windows.net/;FileEndpoint=https://academicar.file.core.windows.net/;TableEndpoint=https://academicar.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=bfqt&srt=c&sp=rwdlacupiytfx&se=2024-06-15T10:04:03Z&st=2024-06-15T02:04:03Z&spr=https&sig=and%2BWbKzZeBXVymd%2FsQQFl7NTqOCPZ%2FcAqYSJ5vz%2BOg%3D';
+        const containerName = 'profile-images';
+
+
+        const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+
+     //   const arrayBuffer = await selectedFile.arrayBuffer();// Fetch the file as an ArrayBuffer
+   
+        const blobName = `${containerName}/${selectedFile.name}`;
+
+        // this uses our container we created earlier
+        let i = 0;
+        let blobs = containerClient.listBlobsFlat();
+        for await (const blob of blobs) {
+
+            console.log(`Blob ${i++}: ${blob.name}`);
+
+
+            // Upload the file
+            const response = await containerClient.uploadBlockBlob(blob.name, selectedFile.type, 1, {
+                blobHTTPHeaders: {
+                    blobContentType: selectedFile.type,
+                    blobContentDisposition: `attachment; filename="${blobName}"`
+                }
+            });
+            return response;
+        }
+
+            console.log('Upload successful');
+           
+            //   await createBlobInContainer(containerClient, selectedFile);
+        
     }
-}
+  //  render()
+
+    return (
+        <div>
+            <table>
+                <thead>
+                <tr>
+                    <th>blob name</th>
+                    <th>blob size</th>
+                    <th>download url</th>
+                </tr>
+                </thead>
+                <tbody>{
+                    <form aria-label="Blob" className="w-full grid grid-cols-12 gap-4" encType="multipart/form-data"
+                          method={"POST"}>
+                        <input type="file" className={"col-span-full"} onChange={handleFileSelection}/>
+                        <Button
+                            variant={"primary"}
+                            text={"Upload Image"}
+                            type={"submit"}
+                            className={"col-span-full"}
+                        />
+
+                    </form>
+                }
+                </tbody>
+            </table>
+
+        </div>
+
+    );
+};
