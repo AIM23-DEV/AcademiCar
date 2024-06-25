294b663eb04d4026b9e86d2a29dd92b8b25a6122
Christoph Wieser <christoph.wieser@edu.fh-joanneum.at>
2024-06-26 00:56:55 +0200

    fix images

--- academicar.client/src/pages/chat/partials/ChatListTile.tsx
+++ academicar.client/src/pages/chat/partials/ChatListTile.tsx
@@ -21,7 +21,7 @@ export const ChatListTile = (props: ChatListTileProps) => {
                 <div className="flex flex-row w-full">
                     {/* Todo load the right avatars */}
                     <ChatAvatar
-                        avatars={isPersonalChat(props.chat) ? ["/src/assets/c5.jpg"] : ["/src/assets/women_mock.jpg", "/src/assets/krucziii.jpg", "/src/assets/c5.jpg", "/src/assets/react.jpg"]}
+                        avatars={isPersonalChat(props.chat) ? ["https://fastly.picsum.photos/id/1012/200/200.jpg?hmac=kENwT0f1ecqbPzBGAw3ITKIrm1xoJdF0oh5tq6nosuM"] : ["https://fastly.picsum.photos/id/1012/200/200.jpg?hmac=kENwT0f1ecqbPzBGAw3ITKIrm1xoJdF0oh5tq6nosuM", "https://fastly.picsum.photos/id/338/200/200.jpg?hmac=5S5SeR5xW8mbN3Ml7wTTJPePX392JafhcFMGm7IFNy0", "https://fastly.picsum.photos/id/338/200/200.jpg?hmac=5S5SeR5xW8mbN3Ml7wTTJPePX392JafhcFMGm7IFNy0", "https://fastly.picsum.photos/id/633/200/200.jpg?hmac=3ZyIOtFWRly1tYi_sTXjhSKzDlB-94qs6KCeIdeiCJo"]}
                         alt="avatar"
                         className="mr-4"/>
                     <div className="flex flex-col w-full">
