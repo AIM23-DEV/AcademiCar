5f8042fb935d2d8ba39d5c222b03366a86e1b64f
Andreas Feichtinger <anfeichtingers@gmail.com>
2024-06-25 14:01:27 +0200

    CST-231: fix facesheet inputs and data, add toast notification on save

--- academicar.client/src/pages/profile/FaceSheetPage.tsx
+++ academicar.client/src/pages/profile/FaceSheetPage.tsx
@@ -2,8 +2,9 @@ import {useEffect, useState} from 'react';
 import {useTranslation} from "react-i18next";
 import {TitleBar} from "../../components/TitleBar";
 import {Button} from "../../components/Buttons";
-import {Select} from "../../components/FormFields";
-import {useParams} from "react-router-dom";
+import {Input} from "../../components/FormFields";
+import {useAuth} from "../../AuthContext.tsx";
+import {Toast} from "../../components/Toast.tsx";
 
 interface PrefsData {
     musicPrefs?: string;
@@ -19,104 +20,95 @@ export const FaceSheetPage = () => {
     const travelLabelText = t('pages/profile:PersonalDataFaceSheet.communication');
     const saveChangesButtonText = t('pages/profile:PersonalDataFaceSheet.save_changes');
 
-    const {loggedInUserId} = useParams();
-    const [prefs, setPrefs] = useState<PrefsData>();
-    const [mPrefs, setMPrefs] = useState<string>();
-    const [iPrefs, setIPrefs] = useState<string>();
-    const [tPrefs, setTPrefs] = useState<string>();
-    const [error, setError] = useState<string | null>();
+    const auth = useAuth();
+    const [mPrefs, setMPrefs] = useState<string>("");
+    const [iPrefs, setIPrefs] = useState<string>("");
+    const [tPrefs, setTPrefs] = useState<string>("");
+    const [error, setError] = useState<string>("");
+    const [showToast, setShowToast] = useState<boolean>(false);
 
     useEffect(() => {
-        fetch(`https://localhost:5173/api/admin/prefs/${loggedInUserId}`)
-            .then(response => response.json())
-            .then((fetchedPrefs: PrefsData) => {
-                setPrefs(fetchedPrefs);
-                setMPrefs(prefs?.musicPrefs);
-                setIPrefs(prefs?.interests);
-                setTPrefs(prefs?.travelPrefs);
-            })
-            .catch(error => {
-                setError(`There was an error fetching preferences: ${error}`);
-                console.error(error);
-            });
-    }, [loggedInUserId]);
+        if (auth.user?.id ?? null) {
+            fetch(`https://localhost:5173/api/admin/prefs/${auth.user?.id}`)
+                .then(response => response.json())
+                .then((fetchedPrefs: PrefsData) => {
+                    setMPrefs(fetchedPrefs?.musicPrefs ?? "");
+                    setIPrefs(fetchedPrefs?.interests ?? "");
+                    setTPrefs(fetchedPrefs?.travelPrefs ?? "");
+                })
+                .catch(error => {
+                    setError(`There was an error fetching preferences: ${error}`);
+                    console.error(error);
+                });
+        }
+    }, [auth.user?.id]);
 
     if (error) return <div>{`There was an error: ${error}`}</div>
 
-    const saveChanges = () => {
+    const saveChanges = (event: any) => {
+        event.preventDefault();
         const newPrefs: PrefsData = {
             musicPrefs: mPrefs,
             interests: iPrefs,
             travelPrefs: tPrefs
         }
 
-        fetch(`https://localhost:5173/api/admin/prefs/update/${loggedInUserId}`, {
+        fetch(`https://localhost:5173/api/admin/prefs/update/${auth.user?.id}`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(newPrefs)
         })
+            .then((response) => {
+                console.log(response);
+                setShowToast(true);
+                setTimeout(() => setShowToast(false), 3000);
+            })
             .catch(error => {
                 setError(`There was an error updating the trip!`);
                 console.error("Error:", error);
             });
-
-        alert('Settings saved');
     }
 
     return (
         <>
             <TitleBar text={pageTitle} hasBackAction/>
 
-            <form className="mt-4 w-full grid grid-cols-12 gap-4" onSubmit={saveChanges}>
-                <Select
+            <form className="w-full flex flex-col space-y-6 mt-6 mb-24" onSubmit={saveChanges}>
+                <Input
                     label={musicTasteLabelText}
                     value={mPrefs}
                     required={true}
                     onChange={(e) => setMPrefs(e.target.value)}
-                    options={{
-                        jazz: 'Jazz',
-                        pop: 'Pop',
-                        schlager: 'Schlager',
-                        techno: 'Techno / Teckno'
-                    }}
                     fullWidth
                     className="col-span-full"
                 />
-                <Select
+                <Input
                     label={interestsLabelText}
                     value={iPrefs}
                     required={true}
                     onChange={(e) => setIPrefs(e.target.value)}
-                    options={{
-                        nature: 'Natur',
-                        cooking: 'Kochen',
-                        travel: 'Reisen',
-                        reading: 'Lesen',
-                        sports: 'Sport'
-                    }}
                     fullWidth
                     className="col-span-full"
                 />
-                <Select
+                <Input
                     label={travelLabelText}
                     value={tPrefs}
                     required={true}
                     onChange={(e) => setTPrefs(e.target.value)}
-                    options={{
-                        talkative: 'Gesprächig',
-                        reserved: 'Zurückhaltend'
-                    }}
                     fullWidth
                     className="col-span-full"
                 />
 
-                <Button
-                    variant="primary"
-                    text={saveChangesButtonText}
-                    textAlign="center"
-                    type="submit"
-                    className="fixed bottom-6 inset-x-6 !w-auto"
-                />
+                {showToast ?
+                    <Toast variant="info" message="Dein Steckbrief wurde gespeichert."
+                           className="fixed bottom-6 inset-x-6 !w-auto"/>
+                    : <Button
+                        variant="primary"
+                        text={saveChangesButtonText}
+                        textAlign="center"
+                        type="submit"
+                        className="fixed bottom-6 inset-x-6 !w-auto"
+                    />}
             </form>
 
         </>
